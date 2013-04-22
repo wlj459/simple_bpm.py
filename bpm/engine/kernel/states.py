621d464490861b5e55f00f4a4848a7df98273ddc
sveinchen <sveinchen@qq.com>
2013-04-22 16:27:43 +0800

    restructured states

--- bpm/engine/kernel/states.py
+++ bpm/engine/kernel/states.py
@@ -5,13 +5,30 @@ bpm.engine.kernel.states
 Built-in task states.
 """
 
-PENDING = 0
-RECEIVED = 1
-STARTED = 2
-SUCCESS = 3
-FAILURE = 4
-REVOKED = 5
-RETRY = 6
-IGNORED = 7
+CREATED = 'CREATED'
+PENDING = 'PENDING'
+READY = 'READY'
+RUNNING = 'RUNNING'
+BLOCKED = 'BLOCKED'
+SUSPENDED = 'SUSPENDED'
+SUCCESS = 'SUCCESS'
+FAILURE = 'FAILURE'
+REVOKED = 'REVOKED'
 
-READY_STATES = []
+TRANSITION = {
+    CREATED:    frozenset([PENDING, REVOKED]),
+    PENDING:    frozenset([READY, REVOKED]),
+    READY:      frozenset([RUNNING, REVOKED, SUSPENDED]),
+    RUNNING:    frozenset([BLOCKED, SUCCESS, FAILURE, REVOKED, SUSPENDED]),
+    BLOCKED:    frozenset([READY, REVOKED, SUSPENDED]),
+    SUSPENDED:  frozenset([READY, REVOKED]),
+    SUCCESS:    frozenset([]),
+    FAILURE:    frozenset([]),
+    REVOKED:    frozenset([]),
+}
+
+
+def can_transit(from_state, to_state):
+    if from_state in TRANSITION:
+        if to_state in TRANSITION[from_state]:
+            return True
