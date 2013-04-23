344bda66d5213b1060cadf77d19739654d2ff088
sveinchen <sveinchen@qq.com>
2013-04-23 17:59:37 +0800

    restructured states and models

--- bpm/engine/kernel/models.py
+++ bpm/engine/kernel/models.py
@@ -1,8 +1,8 @@
 import json
 
-from django.db import models
+from django.db import models, transaction
 
-from . import signals, states
+from . import signals, states, utils
 
 
 class Definition(models.Model):
@@ -29,45 +29,115 @@ class Task(models.Model):
     )
     parent = models.ForeignKey(
         'self',
-        null=True,
         related_name='sub_tasks',
+        null=True,
+        blank=True,
     )
-    ttl = models.PositiveSmallIntegerField(
-        default=0,
-    )
+
     # inputs and outputs
     args = models.TextField()
     kwargs = models.TextField()
     data = models.TextField()
     ex_data = models.TextField()
-    state = models.PositiveSmallIntegerField(
-        choices=tuple(states.x.iteritems()),
-        default=states.PENDING,
+
+    state = models.CharField(
+        max_length=16,
+        choices=zip(states.ALL_STATES, states.ALL_STATES),
+        default=states.CREATED,
     )
-    is_locked = models.BooleanField(
-        default=False,
+    appointment = models.CharField(
+        max_length=16,
+        choices=zip(states.APPOINTMENT_STATES, states.APPOINTMENT_STATES),
+        default='',
+        blank=True,
     )
-    archive = models.TextField()
+
+    _archive = models.TextField()
+    _ack = models.PositiveSmallIntegerField(
+        default=0,
+    )
+    _token = models.SlugField(
+        max_length=6,
+        default=utils.generate_salt(),
+    )
+    _ttl = models.PositiveSmallIntegerField(
+        default=0,
+    )
 
     def __unicode__(self):
-        return self.definition.name
+        return self.name
+
+    def appoint(self, to_state):
+        if to_state in states.APPOINTMENT_STATES:
+            rows = self.__class__.objects.filter(pk=self.pk,
+                                                 _token=self._token)\
+                                         .update(appointment=to_state)
+
+            if rows:
+                self.transit(to_state)
+
+    @transaction.commit_on_success()
+    def _callback(self, return_code=0, data='', ex_data=''):
+        if self.state == states.BLOCKED:
+            kwargs = {}
+            to_state = states.FAILURE
+
+            if return_code:
+                kwargs['data'] = json.dumps(return_code)
+                kwargs['ex_data'] = json.dumps(ex_data)
+            else:
+                kwargs['data'] = json.dumps(data)
+                to_state = states.SUCCESS
+
+            rows = self.__class__.objects.filter(pk=self.pk,
+                                                 _token=self._token)\
+                                         .update(**kwargs)
+
+            if rows:
+                self.transit(to_state)
 
     def callback(self, data):
-        signals.pre_callback.send(sender=self.__class__, instance=self)
+        self._callback(data=data)
 
-        self.__class__.objects.filter(id=self.id, state=states.STARTED)\
-                              .update(state=states.SUCCESS,
-                                      data=json.dumps(data))
+    def errback(self, ex_data, return_code=1):
+        assert return_code == 0
+        self._callback(return_code=return_code, ex_data=ex_data)
 
-        # Signal that the callback is complete
-        signals.post_callback.send(sender=self.__class__, instance=self)
+    @transaction.commit_on_success()
+    def start(self, *args, **kwargs):
+        if self.state == states.CREATED:
+            rows = self.__class__.objects.filter(pk=self.pk,
+                                                 _token=self._token)\
+                                         .update(args=json.dumps(args),
+                                                 kwargs=json.dumps(kwargs))
 
-    def errback(self, ex_data):
-        signals.pre_errback.send(sender=self.__class__, instance=self)
+            if rows:
+                self.transit(states.PENDING)
 
-        self.__class__.objects.filter(id=self.id, state=states.STARTED)\
-                              .update(state=states.FAILURE,
-                                      ex_data=json.dumps(ex_data))
+    def transit(self, to_state):
+        appointment_flag = False
+        if self.appointment:
+            if states.can_transit(self.state, self.appointment):
+                appointment_flag = True
+                if states.State(to_state) < states.State(self.appointment):
+                    to_state = self.appointment
 
-        # Signal that the errback is complete
-        signals.post_errback.send(sender=self.__class__, instance=self)
+        if states.can_transit(self.state, to_state):
+            kwargs = {
+                '_token': utils.generate_salt(),
+                'state': to_state,
+            }
+            if appointment_flag:
+                kwargs['appointment'] = ''
+
+            rows = self.__class__.objects.filter(pk=self.pk,
+                                                 _token=self._token)\
+                                         .update(**kwargs)
+
+            state_change_signal = getattr(signals, 'task_' + to_state.lower())
+            if rows and state_change_signal:
+                instance = self.__class__.objects.get(pk=self.pk)
+                state_change_signal.send(sender=self.__class__, instance=instance)
+
+                if not appointment_flag:
+                    return instance
--- bpm/engine/kernel/states.py
+++ bpm/engine/kernel/states.py
@@ -15,20 +15,105 @@ SUCCESS = 'SUCCESS'
 FAILURE = 'FAILURE'
 REVOKED = 'REVOKED'
 
-TRANSITION = {
-    CREATED:    frozenset([PENDING, REVOKED]),
-    PENDING:    frozenset([READY, REVOKED]),
-    READY:      frozenset([RUNNING, REVOKED, SUSPENDED]),
-    RUNNING:    frozenset([BLOCKED, SUCCESS, FAILURE, REVOKED, SUSPENDED]),
-    BLOCKED:    frozenset([READY, REVOKED, SUSPENDED]),
-    SUSPENDED:  frozenset([READY, REVOKED]),
-    SUCCESS:    frozenset([]),
-    FAILURE:    frozenset([]),
-    REVOKED:    frozenset([]),
-}
+APPOINTMENT_STATES = frozenset([READY, SUSPENDED, REVOKED])
+ALL_STATES = frozenset([CREATED, PENDING, READY, RUNNING,
+                        BLOCKED, SUSPENDED, SUCCESS, FAILURE, REVOKED])
+
+
+class ConstantDict(dict):
+    """ConstantDict is a subclass of :class:`dict`, implementing __setitem__
+    method to avoid item assignment::
+
+    >>> d = ConstantDict({'key': 'value'})
+    >>> d['key'] = 'value'
+    Traceback (most recent call last):
+        ...
+    TypeError: 'ConstantDict' object does not support item assignment
+    """
+
+    def __setitem__(self, key, value):
+        raise TypeError("'%s' object does not support item assignment"
+                        % self.__class__.__name__)
+
+
+_PRECEDENCE = ConstantDict({
+    CREATED: 1,
+    PENDING: 2,
+    READY: 10,
+    RUNNING: 11,
+    BLOCKED: 12,
+    SUSPENDED: 19,
+    SUCCESS: 99,
+    FAILURE: 99,
+    REVOKED: 89,
+})
+
+
+def precedence(state):
+    """Get the precedence for state::
+
+    >>> precedence(PENDING)
+    2
+
+    >>> precedence('UNKNOWN')
+    0
+    """
+    if state in _PRECEDENCE:
+        return _PRECEDENCE[state]
+    else:
+        return 0
+
+
+class State(str):
+    """State is a subclass of :class:`str`, implementing comparison
+    methods adhering to state precedence rules::
+
+        >>> State(PENDING) < State(SUCCESS)
+        True
+
+        >>> State(FAILURE) < State(REVOKED)
+        False
+
+        >>> State(SUCCESS) <= State(FAILURE)
+        True
+    """
+
+    def __gt__(self, other):
+        return precedence(self) > precedence(other)
+
+    def __ge__(self, other):
+        return precedence(self) >= precedence(other)
+
+    def __lt__(self, other):
+        return precedence(self) < precedence(other)
+
+    def __le__(self, other):
+        return precedence(self) <= precedence(other)
+
+
+_TRANSITION = ConstantDict({
+    CREATED: frozenset([PENDING, REVOKED]),
+    PENDING: frozenset([READY, REVOKED]),
+    READY: frozenset([RUNNING, REVOKED, SUSPENDED]),
+    RUNNING: frozenset([BLOCKED, SUCCESS, FAILURE]),
+    BLOCKED: frozenset([READY, REVOKED]),
+    SUSPENDED: frozenset([READY, REVOKED]),
+    SUCCESS: frozenset([]),
+    FAILURE: frozenset([]),
+    REVOKED: frozenset([]),
+})
 
 
 def can_transit(from_state, to_state):
-    if from_state in TRANSITION:
-        if to_state in TRANSITION[from_state]:
+    """Test if :param:`from_state` can transit to :param:`to_state`::
+
+    >>> can_transit(CREATED, PENDING)
+    True
+
+    >>> can_transit(CREATED, READY)
+    False
+    """
+    if from_state in _TRANSITION:
+        if to_state in _TRANSITION[from_state]:
             return True
+    return False
