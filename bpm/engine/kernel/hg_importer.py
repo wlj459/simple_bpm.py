dd961dfac6accf0fdaf99384c51407cd1945765e
Tao Wen <taowen@gmail.com>
2013-07-08 12:08:36 +0800

    import simple_package.simple_module

--- bpm/engine/kernel/hg_importer.py
+++ bpm/engine/kernel/hg_importer.py
@@ -5,16 +5,21 @@ from django.conf import settings
 import mercurial.hg
 import mercurial.ui
 import mercurial.error
+import imp
 
-# signature: path, source_code => module
+# signature: source_code, path => module
 # will be implemented using RestrictedPython
-load_module_hook = None
+compile_hook = None
+# signature: compiled_code, module.__dict__ => void
+exec_hook = None
 
 LOGGER = logging.getLogger(__name__)
 MERCURIAL_SYS_PATH = '__MERCURIAL__'
 
 
 def setUp():
+    assert compile_hook
+    assert exec_hook
     if MERCURIAL_SYS_PATH not in sys.path:
         sys.path.append(MERCURIAL_SYS_PATH)
         sys.path_hooks.append(path_hook)
@@ -32,23 +37,65 @@ def path_hook(path):
 
 class Finder(object):
     def find_module(self, fullname, path=None):
+        # print('find_module', fullname, path)
+        repo_name = fullname.partition('.')[0]
         try:
-            repo = mercurial.hg.repository(mercurial.ui.ui(), '%s/%s/' % (settings.REPO_ROOT, fullname))
-            LOGGER.info('import %s is found as hg repo' % fullname)
+            repo = mercurial.hg.repository(mercurial.ui.ui(), '%s/%s/' % (settings.REPO_ROOT, repo_name))
+            LOGGER.info('import %s is found as hg repo' % repo_name)
             return Loader(repo)
         except mercurial.error.RepoError:
-            LOGGER.debug('import %s not found as hg repo' % fullname)
+            LOGGER.debug('import %s not found as hg repo' % repo_name)
             return None
 
 
 class Loader(object):
     def __init__(self, repo):
         self.repo = repo
+        self.revision = repo['tip']
 
     def load_module(self, fullname):
-        source_code = self.repo['tip']['%s/__init__.py' % fullname].data()
-        if load_module_hook:
-            return load_module_hook(fullname, source_code)
+        # print('load_module', fullname)
+        is_package = self.is_package(fullname)
+        mod = sys.modules.setdefault(fullname, imp.new_module(fullname))
+        mod.__file__ = 'somewhere from __MERCURIAL__' # TODO describe exact location
+        mod.__loader__ = self
+        if is_package:
+            mod.__path__ = [MERCURIAL_SYS_PATH]
+            mod.__package__ = fullname
         else:
-            LOGGER.error('missing load_module_hook')
-            return None
+            mod.__package__ = fullname.rpartition('.')[0]
+        compiled_code = self.get_code(fullname)
+        exec_hook(compiled_code, mod.__dict__)
+        return mod
+
+    def get_code(self, fullname):
+        return compile_hook(self.get_source(fullname), sys.modules[fullname].__file__)
+
+    def is_package(self, fullname):
+        return self._get_source(fullname)[0]
+
+    def get_source(self, fullname):
+        return self._get_source(fullname)[1]
+
+    def _get_source(self, fullname):
+        try:
+            try:
+                return True, self.get_data('%s/__init__.py' % fullname.replace('.', '/'))
+            except IOError:
+                pass
+            try:
+                return False, self.get_data('%s.py' % fullname.replace('.', '/'))
+            except IOError as e:
+                raise ImportError('adapted from IOError: %s' % e)
+        except ImportError:
+            raise
+        except:
+            LOGGER.exception('is_package failed')
+            raise ImportError('adapted from: %s' % sys.exc_info()[1])
+
+    def get_data(self, path):
+        try:
+            return self.revision[path].data()
+        except mercurial.error.ManifestLookupError as e:
+            raise IOError('adapted from ManifestLookupError: %s' % e)
+
--- bpm/engine/kernel/tests/hg_importer_test.py
+++ bpm/engine/kernel/tests/hg_importer_test.py
@@ -1,5 +1,4 @@
 import os
-import imp
 
 from django.test import TestCase
 from django.conf import settings
@@ -7,34 +6,47 @@ from django.conf import settings
 from bpm.engine.kernel import hg_importer
 
 
-def simple_load_module_hook(path, source_code):
+def simple_compile_hook(source_code, path):
 # without using RestrictedPython, so we can focus on the hg_importer logic
-    compiled_code = compile(source_code, path, 'exec')
-    exec_locals = {}
-    exec_globals = dict(__builtins__)
-    exec (compiled_code, exec_globals, exec_locals)
-    module = imp.new_module(path)
-    module.__dict__.update(exec_locals)
-    return module
+    return compile(source_code, path, 'exec', dont_inherit=True)
+
+
+def simple_exec_hook(compiled_code, module_dict):
+    exec (compiled_code, {}, module_dict)
 
 
 class ImportHookTest(TestCase):
     def setUp(self):
         super(ImportHookTest, self).setUp()
-        hg_importer.setUp()
         self.OLD_REPO_ROOT = settings.REPO_ROOT
         settings.REPO_ROOT = os.path.join(os.path.dirname(__file__), 'hg_importer_test_repo_root')
-        self.old_load_module_hook = hg_importer.load_module_hook
-        hg_importer.load_module_hook = simple_load_module_hook
+        self.old_compile_hook = hg_importer.compile_hook
+        hg_importer.compile_hook = simple_compile_hook
+        self.old_exec_hook = hg_importer.exec_hook
+        hg_importer.exec_hook = simple_exec_hook
+        hg_importer.setUp()
 
     def tearDown(self):
         super(ImportHookTest, self).tearDown()
-        settings.REPO_ROOT = self.OLD_REPO_ROOT
-        hg_importer.load_module_hook = self.old_load_module_hook
         hg_importer.tearDown()
+        settings.REPO_ROOT = self.OLD_REPO_ROOT
+        hg_importer.compile_hook = self.old_compile_hook
+        hg_importer.exec_hook = self.old_exec_hook
 
 
     def test_simple_package(self):
         import simple_package
 
+        self.assertTrue(hasattr(simple_package, '__path__'))
+        self.assertEqual('simple_package', simple_package.__name__)
+        self.assertEqual('simple_package', simple_package.__package__)
         self.assertEqual('world', simple_package.hello)
+
+    def test_simple_module(self):
+        import simple_package
+        import simple_package.simple_module
+
+        self.assertFalse(hasattr(simple_package.simple_module, '__path__'))
+        self.assertEqual('simple_package.simple_module', simple_package.simple_module.__name__)
+        self.assertEqual('simple_package', simple_package.simple_module.__package__)
+        self.assertEqual('world', simple_package.simple_module.hello)
