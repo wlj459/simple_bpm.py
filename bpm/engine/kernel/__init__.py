48c3837e4205e4efe2349191c5abb2870a52706b
Tao Wen <taowen@gmail.com>
2013-07-09 17:45:32 +0800

    features is not being used

--- bpm/engine/kernel/__init__.py
+++ bpm/engine/kernel/__init__.py
@@ -5,7 +5,6 @@ from . import receivers, celery_signals
 from .backends.base import BaseTaskBackend
 from .backends.component import BaseComponent
 from .backends.process import BaseProcess
-from .features import internal_import
 
 
 modules = {}
